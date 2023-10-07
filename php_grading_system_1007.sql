b1dc0fdbcc05ea2d04fdc37d942e4c95487fa62c
jaylingers <jomariejaybatingal@gmail.com>
2023-10-07 20:22:31 +0800

    update db

---
+++ php_grading_system_1007.sql
@@ -0,0 +1,422 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2023 at 02:21 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `php_grading_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade_info`
+--
+
+CREATE TABLE IF NOT EXISTS `grade_info` (
+  `id` int(11) NOT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `section` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `grade_info`
+--
+
+INSERT INTO `grade_info` (`id`, `grade`, `section`) VALUES
+(1, 1, 'gumamela');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_visited_info`
+--
+
+CREATE TABLE IF NOT EXISTS `page_visited_info` (
+  `id` int(11) NOT NULL,
+  `user_id` int(25) DEFAULT NULL,
+  `date_visited` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promoted_students`
+--
+
+CREATE TABLE IF NOT EXISTS `promoted_students` (
+  `id` int(11) NOT NULL,
+  `student_lrn` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promoted_students_history`
+--
+
+CREATE TABLE IF NOT EXISTS `promoted_students_history` (
+  `id` int(11) NOT NULL,
+  `student_lrn` varchar(50) DEFAULT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `section` varchar(25) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school_years_info`
+--
+
+CREATE TABLE IF NOT EXISTS `school_years_info` (
+  `id` int(11) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `current` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_enrollment_info`
+--
+
+CREATE TABLE IF NOT EXISTS `students_enrollment_info` (
+  `id` int(11) NOT NULL,
+  `students_info_lrn` varchar(50) NOT NULL,
+  `grade` int(50) NOT NULL,
+  `section` varchar(50) NOT NULL,
+  `school_year` int(25) NOT NULL,
+  `date_enrolled` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `grade_status` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_grade_info`
+--
+
+CREATE TABLE IF NOT EXISTS `students_grade_info` (
+  `id` int(11) NOT NULL,
+  `teacher_lrn` varchar(25) NOT NULL,
+  `student_lrn` varchar(25) DEFAULT NULL,
+  `subject` varchar(25) NOT NULL,
+  `grade_level` varchar(25) NOT NULL,
+  `first_grade` int(11) DEFAULT NULL,
+  `second_grade` int(11) DEFAULT NULL,
+  `third_grade` int(11) DEFAULT NULL,
+  `fourth_grade` int(11) DEFAULT NULL,
+  `final` int(11) DEFAULT NULL,
+  `status` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_info`
+--
+
+CREATE TABLE IF NOT EXISTS `students_info` (
+  `id` int(4) NOT NULL,
+  `lrn` varchar(255) NOT NULL,
+  `f_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `m_name` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `b_date` date NOT NULL,
+  `b_place` varchar(50) NOT NULL,
+  `c_status` varchar(50) NOT NULL,
+  `age` int(3) NOT NULL,
+  `nationality` varchar(50) NOT NULL,
+  `religion` varchar(50) NOT NULL,
+  `contact_number` varchar(50) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `home_address` varchar(50) NOT NULL,
+  `guardian_name` varchar(50) NOT NULL,
+  `addedBy` int(11) NOT NULL,
+  `teacher_lrn` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_available_info`
+--
+
+CREATE TABLE IF NOT EXISTS `subject_available_info` (
+  `id` int(11) NOT NULL,
+  `grade_level` varchar(25) DEFAULT NULL,
+  `subject` varchar(25) DEFAULT NULL,
+  `available_number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_list_info`
+--
+
+CREATE TABLE IF NOT EXISTS `subject_list_info` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `applicable_for` varchar(25) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers_info`
+--
+
+CREATE TABLE IF NOT EXISTS `teachers_info` (
+  `id` int(11) NOT NULL,
+  `lrn` varchar(25) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `civil_status` varchar(25) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `section` varchar(25) NOT NULL,
+  `grade` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers_subject_info`
+--
+
+CREATE TABLE IF NOT EXISTS `teachers_subject_info` (
+  `id` int(11) NOT NULL,
+  `teachers_info_lrn` varchar(25) NOT NULL,
+  `subject` varchar(25) NOT NULL,
+  `room` varchar(25) NOT NULL,
+  `grade_level` varchar(25) NOT NULL,
+  `schedule_time_in` time NOT NULL,
+  `schedule_time_out` time NOT NULL,
+  `schedule_day` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trash_info`
+--
+
+CREATE TABLE IF NOT EXISTS `trash_info` (
+  `id` int(11) NOT NULL,
+  `user_lrn` varchar(25) DEFAULT NULL,
+  `name` varchar(25) NOT NULL,
+  `history` varchar(9999) DEFAULT NULL,
+  `removed_date` date NOT NULL,
+  `removed_by` varchar(25) NOT NULL,
+  `position` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_info`
+--
+
+CREATE TABLE IF NOT EXISTS `users_info` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `user_type` varchar(25) NOT NULL,
+  `user_lrn` varchar(25) NOT NULL,
+  `img_path` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users_info`
+--
+
+INSERT INTO `users_info` (`id`, `last_name`, `first_name`, `username`, `password`, `email`, `user_type`, `user_lrn`, `img_path`) VALUES
+(1, 'lastname', 'admin', 'admin', 'admin', 'jaylingers@gmail.com', 'admin', '', '../../assets/users_img/noImage.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `grade_info`
+--
+ALTER TABLE `grade_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `page_visited_info`
+--
+ALTER TABLE `page_visited_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `promoted_students`
+--
+ALTER TABLE `promoted_students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `promoted_students_history`
+--
+ALTER TABLE `promoted_students_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `school_years_info`
+--
+ALTER TABLE `school_years_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students_enrollment_info`
+--
+ALTER TABLE `students_enrollment_info`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `students_grade_info`
+--
+ALTER TABLE `students_grade_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students_info`
+--
+ALTER TABLE `students_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject_available_info`
+--
+ALTER TABLE `subject_available_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject_list_info`
+--
+ALTER TABLE `subject_list_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers_info`
+--
+ALTER TABLE `teachers_info`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `teachers_subject_info`
+--
+ALTER TABLE `teachers_subject_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trash_info`
+--
+ALTER TABLE `trash_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_info`
+--
+ALTER TABLE `users_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `grade_info`
+--
+ALTER TABLE `grade_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `page_visited_info`
+--
+ALTER TABLE `page_visited_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=74;
+--
+-- AUTO_INCREMENT for table `promoted_students`
+--
+ALTER TABLE `promoted_students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `promoted_students_history`
+--
+ALTER TABLE `promoted_students_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `school_years_info`
+--
+ALTER TABLE `school_years_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `students_enrollment_info`
+--
+ALTER TABLE `students_enrollment_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `students_grade_info`
+--
+ALTER TABLE `students_grade_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `students_info`
+--
+ALTER TABLE `students_info`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `subject_available_info`
+--
+ALTER TABLE `subject_available_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subject_list_info`
+--
+ALTER TABLE `subject_list_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teachers_info`
+--
+ALTER TABLE `teachers_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `teachers_subject_info`
+--
+ALTER TABLE `teachers_subject_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `trash_info`
+--
+ALTER TABLE `trash_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users_info`
+--
+ALTER TABLE `users_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
